362ef65c6ef1026ef4a8da5d82c4c1bf41ac4573
Mihdan Advani <mihdan31@gmail.com>
2023-08-09 12:08:20 +0700

    tambah alert

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { GraphQlClient } from "../../API/Graph";
 import { ThemeProvider } from "next-themes";
-import ThemeSwitcher from "@/app/ThemeSwitcher";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
